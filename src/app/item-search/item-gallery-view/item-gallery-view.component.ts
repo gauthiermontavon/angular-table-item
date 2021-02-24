8b21b1a33ac1fc9390b278b0735dd54b6c7d4144
gauthiermontavon <gauthier.montavon@gmail.com>
2021-02-24 15:55:23 +0100

    2 binaries uploaded

--- src/app/item-search/item-gallery-view/item-gallery-view.component.ts
+++ src/app/item-search/item-gallery-view/item-gallery-view.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Item } from "../../interfaces/item";
 import { ItemDatasource } from "../../services/item-datasource";
 
 @Component({
-  selector: 'app-item-gallery-view',
-  templateUrl: './item-gallery-view.component.html',
-  styleUrls: ['./item-gallery-view.component.css']
+  selector: "app-item-gallery-view",
+  templateUrl: "./item-gallery-view.component.html",
+  styleUrls: ["./item-gallery-view.component.css"]
 })
 export class ItemGalleryViewComponent implements OnInit {
-
-  @Input('data')
+  @Input("data")
   dataSource: ItemDatasource;
+  itemsList: Item[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log("INIT gallery:" + this.dataSource);
 
-    this.dataSource.totalCount
+    this.dataSource.itemsSubject.subscribe(val => {
+      this.itemsList = val;
+      val.forEach(item => {
+        console.log("item.id" + item.id);
+        console.log("item.title" + item.title);
+      });
+      console.log("subscribe itemsSubject of datssoure:" + val);
+    });
   }
-
 }
